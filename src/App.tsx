f9fcdc397b20224feef250d756610c147bdd6cb7
Arsalan Rabdanov <arsalan.rabdanov2078@gmail.com>
2025-11-20 15:30:48 -0500

    starter scss added

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,16 @@
+import css from "./App.module.scss";
+
 export default function App() {
-  return <div>App</div>;
+  const sentence = `Sunrise, parabellum.`;
+
+  return (
+    <div className={css.app}>
+      <div data-text-preset="1">{sentence}</div>
+      <div data-text-preset="2">{sentence}</div>
+      <div data-text-preset="3">{sentence}</div>
+      <div data-text-preset="4">{sentence}</div>
+      <div data-text-preset="4-bold">{sentence}</div>
+      <div data-text-preset="5">{sentence}</div>
+    </div>
+  );
 }
